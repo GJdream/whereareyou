b68ed6c2c47b1cbc34d93435dbdd220644963464
Saravana Vijay <saravana.vijay.kumar@gmail.com>
2013-11-30 17:22:31 +0530

    Initial Commit

---
+++ whereareyou/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  whereareyou
+//
+//  Created by Saravana vijaya kumar Amirthalingam on 30/11/13.
+//  Copyright (c) 2013 hackatron13. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ whereareyou/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  whereareyou
+//
+//  Created by Saravana vijaya kumar Amirthalingam on 30/11/13.
+//  Copyright (c) 2013 hackatron13. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
